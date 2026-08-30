d9352dd5e707c318a55ef810bab059f483d104fc
Romlord14495 <eddiebob146@gmail.com>
2026-08-29 23:17:25 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #OrangeFox/TWRP Config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_nevada.mk \
-    $(LOCAL_DIR)/omni_nevada.mk
+    $(LOCAL_DIR)/twrp_utah.mk \
+    $(LOCAL_DIR)/omni_utah.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_nevada-user \
-    twrp_nevada-userdebug \
-    twrp_nevada-eng \
-    omni_nevada-user \
-    omni_nevada-userdebug \
-    omni_nevada-eng
+    twrp_utah-user \
+    twrp_utah-userdebug \
+    twrp_utah-eng \
+    omni_utah-user \
+    omni_utah-userdebug \
+    omni_utah-eng
